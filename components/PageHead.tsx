3d46b9eda598c7104b1f09dafaf031649636327d
Patrick Kim <79458980+sleeeeptight@users.noreply.github.com>
2025-03-12 10:53:27 +0900

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -50,7 +50,8 @@ export function PageHead({
       />
 
       <meta name='robots' content='index,follow' />
-      <meta name='google-site-verification' content='73W0XZpWx3ZP0Ja5Sc-hAisxfYLtQa7PjylUvnFumWg' />
+    > dig nooroongjwi.vercel.app txt +short
+"google-site-verification=73W0XZpWx3ZP0Ja5Sc-hAisxfYLtQa7PjylUvnFumWg -all"
       <meta name='naver-site-verification' content='1363029847c47228dce96330cf8763445c282da6' />
       <meta property='og:type' content='website' />
 
