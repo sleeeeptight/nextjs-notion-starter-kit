81dbf5d5189d5dceaf550805ccf7810627437bd9
Patrick Kim <79458980+sleeeeptight@users.noreply.github.com>
2025-03-13 13:15:32 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Nooroongjwi',
 
   // open graph metadata (optional)
-  description: 'Nooroongjwi Hose',
+  description: 'Nooroongjwi Hose 누룽쥐네 블로그 입니다.',
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
